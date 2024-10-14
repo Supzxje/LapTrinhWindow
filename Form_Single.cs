23ee3a3646187deb1bff15b753aa1b7269cf639f
Supzxje <141908864+Supzxje@users.noreply.github.com>
2024-10-14 17:58:19 +0700

    Add files via upload

---
+++ Form_Single.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Game_LatThe
+{
+    public partial class Form_Single : Form
+    {
+        public Form_Single()
+        {
+            InitializeComponent();
+        }
+    }
+}
